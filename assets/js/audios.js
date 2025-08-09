f7b085ac9d8823b6fd55941323d07438bb6e3851
nneliifeanyi <stanvicbest@gmail.com>
2025-08-09 10:00:23 +0100

    day3

---
+++ assets/js/audios.js
@@ -0,0 +1,25 @@
+// Song titles
+
+const audios24 = [
+	['FORCESES OF THE KINGDOM', 'Wisdom Eze',],
+	['JESUS THE PATH TO OUR GLORY', 'Isreal Egwu',],
+	['THE VAIN GLORY DAY3', 'Nicholas Ogbonna',],
+	['THE FUTURE GLORY', 'Wisdom Eze',],
+	['Behold His Glory', 'Wisdom Eze',],
+];
+
+
+const audios25 = [
+	['WHEN HE COMES - TALK 1', '',],
+	['WALKING IN HIS STEPS - TALK 2', '',],
+	['THE HIDDEN TREASURE - TALK 3', '',],
+	['WHILE MEN SLEPT - TALK 4', '',],
+	['DAY 2 - TALK 1', '',],
+	['DAY 2 - TALK 2', '',],
+	['DAY 2 - TALK 3', '',],
+	['DAY 2 - TALK 4', '',],
+	['DAY 3 - TALK 1', '',],
+	['DAY 3 - TALK 2', '',],
+	['DAY 3 - TALK 3', '',],
+	['DAY 3 - TALK 4', '',]
+];
